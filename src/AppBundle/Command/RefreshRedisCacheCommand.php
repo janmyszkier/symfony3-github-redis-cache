c5d130473aa599b9be4e282f58ddbe9cd75a29a9
BARTOSZ WAWRZYŃCZYK <bartoszwawrzynczyk@MacBook-Pro-BARTOSZ.local>
2020-02-20 20:49:39 +0100

    Update cache strategy

--- src/AppBundle/Command/RefreshRedisCacheCommand.php
+++ src/AppBundle/Command/RefreshRedisCacheCommand.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\ApiCache\Strategy\GithubCacheStrategy;
+use AppBundle\Repository\IssuesRepository;
 use Doctrine\Common\Cache\PredisCache;
+use GuzzleHttp\HandlerStack;
 use Kevinrob\GuzzleCache\CacheEntry;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
 use Predis\Client;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -25,28 +29,50 @@ class RefreshRedisCacheCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Checking for redis keys');
-        if(empty(getenv('REDIS_URL'))){
+        if (empty(getenv('REDIS_URL'))) {
             putenv('REDIS_URL=tcp://127.0.0.1:6379?database=0&persistent=1');
         }
+
+        $stack = HandlerStack::create();
+        $cacheMiddleware = new CacheMiddleware(
+            new GithubCacheStrategy(
+                new DoctrineCacheStorage(
+                    new PredisCache(
+                        new \Predis\Client(getenv('REDIS_URL'))
+                    )
+                )
+            )
+        );
+
+        $client = new \GuzzleHttp\Client();
+        $issuesRepository = new IssuesRepository($stack, $cacheMiddleware, $client);
 
         $redis = new Client(
             getenv('REDIS_URL')
         );
 
         $keys = $redis->keys('\[github*');
-        foreach($keys as $key){
+        foreach ($keys as $key) {
             $redisValue = $redis->get($key);
             $cacheKey = unserialize($redisValue);
             $cacheEntry = unserialize($cacheKey);
+
             /* @var $cacheEntry CacheEntry */
-
             $expiry = $cacheEntry->getStaleAt()->getTimestamp();
-            $remainingSeconds = $expiry-time();
+            $remainingSeconds = $expiry - time();
 
-            if($remainingSeconds < 300 ){
-                $output->writeln($key.' should be recached ('.$remainingSeconds.' s left)');
+            if ($remainingSeconds < 300) {
+                $output->writeln($key . ' should be recached (' . $remainingSeconds . ' s left)');
+
+                $apiKey = $cacheEntry->getOriginalRequest()->getHeaders()['Authorization'][0];
+                $apiKey = str_replace('token ', '', $apiKey);
+                $uri = $cacheEntry->getOriginalRequest()->getUri();
+                $urlToRecall = $uri->getScheme() . '://' . $uri->getHost() . $uri->getPath() . '?' . $uri->getQuery();
+
+                $cacheMiddleware->getCacheStorage()->delete($cacheEntry->getOriginalRequest());
+                $issuesRepository->callGithubUrl($urlToRecall, $apiKey, 'GET', false);
             } else {
-                $output->writeln($key.' should NOT be recached ('.$remainingSeconds.' s left)');
+                $output->writeln($key . ' should NOT be recached (' . $remainingSeconds . ' s left)');
             }
         }
     }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,15 +3,14 @@
 namespace AppBundle\Controller;
 
 use AppBundle\ApiCache\Strategy\GithubCacheStrategy;
+use AppBundle\Repository\IssuesRepository;
 use Doctrine\Common\Cache\PredisCache;
 use Exception\ApiException;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use Kevinrob\GuzzleCache\CacheMiddleware;
 use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +21,28 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $issues = $this->getIssues();
+        if (empty(getenv('REDIS_URL'))) {
+            putenv('REDIS_URL=tcp://127.0.0.1:6379?database=0&persistent=1');
+        }
+
+        $stack = HandlerStack::create();
+        $cacheMiddleware = new CacheMiddleware(
+            new GithubCacheStrategy(
+                new DoctrineCacheStorage(
+                    new PredisCache(
+                        new \Predis\Client(getenv('REDIS_URL'))
+                    )
+                )
+            )
+        );
+
+        $client = new Client();
+        $issuesRepository = new IssuesRepository($stack, $cacheMiddleware, $client);
+        try {
+            $issues = $issuesRepository->getIssues('');
+        } catch (\Exception $exception) {
+            die($exception->getMessage());
+        }
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
@@ -30,148 +50,4 @@ class DefaultController extends Controller
             'issues' => $issues,
         ]);
     }
-
-    public function callGithubUrl($url, $method = 'GET', $params = null)
-    {
-        if(empty(getenv('REDIS_URL'))){
-            putenv('REDIS_URL=tcp://127.0.0.1:6379?database=0&persistent=1');
-        }
-
-        $apiToken ='';
-        $headers = [
-            'Authorization' => 'token '.$apiToken,
-            'User-Agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
-            'Accept' => 'application/json',
-        ];
-        // Create default HandlerStack
-        $stack = HandlerStack::create();
-        if(!empty(getenv('REDIS_URL'))){
-            $stack->push(
-                new CacheMiddleware(
-                    new GithubCacheStrategy(
-                        new DoctrineCacheStorage(
-                            new PredisCache(
-                                new \Predis\Client(getenv('REDIS_URL'))
-                            )
-                        )
-                    )
-                ),
-                'cache'
-            );
-        }
-
-        $client = new \GuzzleHttp\Client([
-            'handler' => $stack,
-            'headers'=> $headers
-        ]);
-
-        if (!$apiToken) {
-            throw new \Exception('API token missing', 401);
-        }
-
-        try {
-            $options = [
-                /* re-adding handler here is necessary for \GuzzleHttp\Client::transfer to pick it up */
-                'handler' => $stack,
-                'headers' => $headers,
-            ];
-            $response = $client->request($method, $url,$options);
-        } catch (\Exception $e){
-            var_dump($url);
-            var_dump($method);
-            //var_dump($options);
-            die($e->getMessage());
-        }
-
-        $currentPageItems = json_decode($response->getBody());
-        if (!is_array($currentPageItems)) {
-            if (isset($currentPageItems->message)) {
-                die($currentPageItems->message);
-            }
-            $currentPageItems = [$currentPageItems];
-        }
-
-        $allPageItems = [];
-
-        $allPageItems = array_merge($allPageItems,$currentPageItems);
-
-        $nextPageItems = null;
-        $nextPageUrl = null;
-
-        $responseHeaders = $response->getHeaders();
-
-        $nextPageItems = null;
-        if(!empty($responseHeaders['Link'])) {
-            if (stristr($responseHeaders['Link'][0], '; rel="next"') > -1) {
-                $headerLinks = explode(',', $responseHeaders['Link'][0]);
-                foreach ($headerLinks as $headerLink) {
-                    if (stristr($headerLink, '; rel="next"') === -1) {
-                        continue;
-                    }
-
-                    $pattern = '#<(.*?)>; rel="next"#m';
-                    preg_match($pattern, $headerLink, $linkResponseHeaders);
-                    if ($linkResponseHeaders) {
-                        $nextPageUrl = $linkResponseHeaders[1];
-                        $nextPageItems = $this->callGithubUrl($nextPageUrl);
-                    }
-                }
-            }
-        }
-
-        if($nextPageItems) {
-            $allPageItems = array_merge($allPageItems,$nextPageItems);
-        } else {
-            $allPageItems = $currentPageItems;
-        }
-
-        return $allPageItems;
-    }
-
-    function getIssues()
-    {
-        $allOrganizationIssues = [];
-
-        //normal issues
-        $url = 'https://api.github.com/issues?filter=all';
-        $userIssues = $this->callGithubUrl($url);
-
-        //get organizations
-        $url = 'https://api.github.com/user/orgs?';
-        $organizations = $this->callGithubUrl($url);
-
-        //and each of their issues
-        $organizationIssuesWithRepositoryInfo =[];
-        $userIssuesArray =[];
-
-        foreach($organizations as $org) {
-            if(!$org) {
-                continue;
-            }
-
-            $url = $org->repos_url;
-
-            $thisOrganizationRepos = $this->callGithubUrl($url);
-            foreach ($thisOrganizationRepos as $thisOrganizationRepo) {
-                if($thisOrganizationRepo == 'Bad credentials'){
-                    throw new Exception('Bad credentials');
-                }
-
-                $issuesUrl = $thisOrganizationRepo->issues_url;
-                $issuesUrl = str_replace('{/number}', '', $issuesUrl);
-                $thisOrganizationIssues = $this->callGithubUrl($issuesUrl);
-
-                foreach ($thisOrganizationIssues as $originalOrganizationIssues) {
-                    $originalOrganizationIssues -> repoInfo = $thisOrganizationRepo;
-                    array_push($organizationIssuesWithRepositoryInfo, $originalOrganizationIssues);
-                }
-
-                if($thisOrganizationIssues !== null) {
-                    $allOrganizationIssues = array_merge($allOrganizationIssues, $organizationIssuesWithRepositoryInfo);
-                }
-            }
-        }
-        return array_merge($userIssues,$allOrganizationIssues);
-    }
-
 }
